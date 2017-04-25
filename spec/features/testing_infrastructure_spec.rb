20f7801e1e27c672fc6aecc3c2125b1d4cf5639e
Unai Motriko <unaimotriko@gmail.com>
2017-04-25 16:29:18 +0100

    Reveals HP

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,10 +1,17 @@
 
 feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+  scenario 'returns page with names' do
     visit('/')
     fill_in :player_1_name, with: 'Unai'
     fill_in :player_2_name, with: 'Sulaiman'
     click_button 'Submit'
     expect(page).to have_content 'Unai vs. Sulaiman'
+  end
+  scenario 'Shows HP' do
+    visit('/')
+    fill_in :player_1_name, with: 'Unai'
+    fill_in :player_2_name, with: 'Sulaiman'
+    click_button 'Submit'
+    expect(page).to have_content '100/100HP'
   end
 end
