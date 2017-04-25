f2e12dde9e15877bdb2882dcfbc1706bfd47882e
Unai Motriko <unaimotriko@gmail.com>
2017-04-25 13:10:28 +0100

    It runs a sinatra modular battle app

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
+
 require 'sinatra/base'
 require 'shotgun'
 
-class MyApp < Sinatra::Application
+class Battle < Sinatra::Base
+  set :session_secret, 'here be dragons'
+
   get '/' do
     'Hello world!'
   end
+
+  get '/test' do
+    'shotgun'
+  end
+
 end
