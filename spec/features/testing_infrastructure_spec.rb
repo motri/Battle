2c393a79f90a5eef39d132cc75f68c975d16078e
Unai Motriko <unaimotriko@gmail.com>
2017-04-25 16:37:00 +0100

    Refactors tests

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,17 +1,11 @@
 
 feature 'Testing infrastructure' do
   scenario 'returns page with names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Unai'
-    fill_in :player_2_name, with: 'Sulaiman'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Unai vs. Sulaiman'
   end
   scenario 'Shows HP' do
-    visit('/')
-    fill_in :player_1_name, with: 'Unai'
-    fill_in :player_2_name, with: 'Sulaiman'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content '100/100HP'
   end
 end
