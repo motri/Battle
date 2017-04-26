00f1dd93f64a662637e9c2a2c11eccf947541eca
Unai Motriko <unaimotriko@gmail.com>
2017-04-26 22:13:40 +0100

    It runs RSpec tests on game.rb and passes them

---
+++ spec/game_spec.rb
@@ -0,0 +1,17 @@
+
+require 'player'
+
+describe Player do
+  let(:player) { described_class.new(:peter) }
+  let(:player2) { described_class.new(:unai) }
+  describe '#name' do
+    it 'assigns a player name when we initialize' do
+      expect(player.name).to eq :peter
+    end
+  end
+  describe '#hp' do
+    it 'assigns a player health points' do
+      expect(player.hp).to eq described_class::HP_DEFAULT
+    end
+  end
+end
